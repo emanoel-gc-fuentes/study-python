bcf1f18153755f6ba8fff986095f3f509fa2cb13
emanoel-ciel <emanoel.cardoso@ciel-it.com>
2024-03-04 17:01:29 -0300

    add

---
+++ iniciante/aula_023/listas.py
@@ -0,0 +1,12 @@
+# TEMA: Listas mutáveis em Python
+# type: list / method: list()
+# Suporta vários valores  de qualquer tipo;
+
+# Métodos frequentemente utilizados: append(), insert(), pop(), del(), clear(), extend() ...
+
+string = 'TEXTO'
+
+# sintax: variável = []
+
+l_data = [123, True, 'Emanoel', 1.5, ['teste', 'abc']] # ''
+print(l_data)
